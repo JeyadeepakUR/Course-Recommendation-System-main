75ff00fd0a7618c565ccba38276820ff2c8d33d0
JeyadeepakUR <jeyadeepak2005@gmail.com>
2023-10-19 21:59:04 +0530

    edit search bar

--- main.py
+++ main.py
@@ -16,19 +16,24 @@ def recommend():
     courses_list = pickle.load(open('courses.pkl','rb'))
     courses_list2 = pickle.load(open('udem_courses.pkl','rb'))
     final_courses = pd.concat([courses_list, courses_list2], ignore_index=True)
+    print(final_courses.head())
     with open("similarity.pkl", "rb") as f:
         data1 = pickle.load(f)
     with open("udem_similarity.pkl", "rb") as f:
         data2 = pickle.load(f)
     final_data = np.vstack((data1, data2))    
+    
     def recommend(course):
-        index = final_courses[final_courses['course_name'] == course].index[0]
-        distances = sorted(list(enumerate(final_data[index])), reverse=True, key=lambda x: x[1])
-        recommended_course_names = []
-        for i in distances[1:7]:
-            course_name = final_courses.iloc[i[0]].course_name
-            recommended_course_names.append(course_name)
+        search_terms = set(course.split())
 
+        similarity_scores = {}
+        for i,r in final_courses.iterrows():
+            course_name = r['course_name']
+            course_tags = set(r['tags'].split())
+            similarity_scores[course_name] = len(search_terms.intersection(course_tags))
+
+        recommended_course_names = sorted(similarity_scores, key=lambda x: similarity_scores[x], reverse=True)[:7]
+        #print(recommended_course_names)
         return recommended_course_names
 
     course_list = final_courses['course_name'].values
@@ -37,10 +42,10 @@ def recommend():
     recommended_course_names = recommend(selected_course)  
     recommended_courses_html = ""
     for course in recommended_course_names:
-        recommended_courses_html += f"<li>{{course}}</li>"
+        recommended_courses_html += f"<li>{course}</li>"
 
     return render_template('index.html', selected_course=selected_course, recommended_courses=recommended_courses_html)
-
+    #return render_template('index.html', courses=[], recommended_courses=[])
 @app.route('/about')
 def about():
     return render_template('about.html')
